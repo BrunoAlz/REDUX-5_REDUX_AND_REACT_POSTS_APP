cfd6727baef605436b707d024b1fa5e681927a21
Bruno Alves <bruno6821@gmail.com>
2023-01-23 23:46:32 +0100

    Implementação inicial dos reducers

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -5,6 +5,23 @@ const initialState = {
   posts: [],
   post: {},
 };
-// Actions
+// Actions Creators
+// 1 Fetch Posts (request started, success, error)
+// Request Started
+const fetchPostsRequest = () => {
+  return {
+    type: "FETCH_POSTS_REQUEST",
+  };
+};
+
+// Success
+const fetchPostSuccess = () => {
+  return {
+    type: "FETCH_POSTS_SUCCESS",
+  };
+};
+
+// 2 Fetch post (request started, success, error)
+const fetchPostAction = () => {};
 // Reducers
 // Store
